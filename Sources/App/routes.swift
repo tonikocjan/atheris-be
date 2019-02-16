aeae8bbdf21aea86cbaad42b0f82e005bab50727
Toni Kocjan <toni.kocjan@poviolabs.com>
2019-02-16 16:26:45 +0100

    Persist output

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,20 @@
 import Vapor
 
-var buffer = ""
+struct Output: Encodable {
+  var compilerName = "atheris"
+  var compilerVersion = "0.1.2"
+  var consoleStack = [String]()
+  var racketCode: String? = ""
+  var smlCode: String? = ""
+  var error: String? = ""
+}
+
+var requestOutput = Output()
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
   router.get { req in
-    return try req.view().render("compiler")
+    return try req.view().render("compiler", requestOutput)
   }
   
   router.post(CompileRequest.self) { req, compile -> Future<View> in
@@ -15,30 +24,30 @@ public func routes(_ router: Router) throws {
     let code = compile.code.replacingOccurrences(of: "\r", with: "")
     #endif
     let stream = TextStream(string: code)
+    requestOutput.smlCode = code
     
     do {
       let atheris = try Atheris(inputStream: stream)
       let output = try atheris.compile() as! TextOutputStream
       let fileOutputStream = FileOutputStream(fileWriter: try FileWriter(fileUrl: URL(string: "code")!))
       fileOutputStream.print(output.buffer)
+      requestOutput.racketCode = output.buffer
       
       let promise = req.eventLoop.newPromise(View.self)
-      try Executor().execute(file: "code") { result -> Void in
-        buffer = "atheris [0.0.1 (pre-alpha)]: " + result + "\n" + buffer
+      try Executor().execute(file: "code") { result in
+        requestOutput.consoleStack.insert(result, at: 0)
+        requestOutput.error = nil
         _ = try? req
           .view()
-          .render("compiler", ["output": output.buffer,
-                               "code": code,
-                               "result": buffer])
+          .render("compiler", requestOutput)
           .do { promise.succeed(result: $0) }
       }
       return promise.futureResult
     } catch {
       let errorMessage = (error as? AtherisError)?.errorMessage ?? error.localizedDescription.replacingOccurrences(of: "\n", with: "\r\n")
-      
-      return try req.view().render("compiler",
-                                   ["error": errorMessage,
-                                    "code": code])
+      requestOutput.error = errorMessage
+      requestOutput.racketCode = nil
+      return try req.view().render("compiler", requestOutput)
     }
   }
 }
